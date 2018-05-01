fa8abb81c7eb97266fad703f2bd79dd6b2cfd454
Rodolfo Yabut <rodolfoyabut@gmail.com>
2018-05-01 10:19:50 -0700

    Pass all trx pool tests

--- part2/transaction-pool.js
+++ part2/transaction-pool.js
@@ -1,12 +1,33 @@
 "use strict";
 
-// var TransactionPool = Object.assign(module.exports,{
-//    TODO
-// });
+var TransactionPool = Object.assign(module.exports,{
+  pending: [],
+  invalid: [],
+  findTransaction: (txHash, collection) => {
+    let result
+    collection.forEach((tx) => {
+      if (tx.hash === txHash) {
+        result = tx
+      }
+    })
 
+    return result
+  },
+  isPending: (hash) => this.findTransaction(hash, this.pending),
+  isInvalid: (hash) => this.findTransaction(hash, this.invalid),
+  accept: (transaction) => {
+    if (!transaction.data) {
+      this.invalid.push(transaction)
+      return false
+    }
 
-// Hints:
-//
-// isPending(..)
-// isInvalid(..)
-// accept(..)
+    if (!this.isPending(transaction.hash) && !this.isInvalid(transaction.hash)) {
+      this.pending.push(transaction)
+      return true
+    } else {
+      this.invalid.push(transaction)
+    }
+
+    return false
+  }
+});
